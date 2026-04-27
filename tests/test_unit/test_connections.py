6baa711e35e2dba80618a971caf3bbfa569f0ab3
willGraham01 <ccaegra@ucl.ac.uk>
2026-04-27 12:31:10 +0100

    Basic test for re-indexing case

--- tests/test_unit/test_connections.py
+++ tests/test_unit/test_connections.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pandas as pd
+
 from brainglobe_data_api_connectivity.connections import Connections
 
 
@@ -17,3 +19,39 @@ def test_me(
     G = Connections.from_files(node_file, edge_table, edge_meta)
 
     pass
+
+
+def test_setup_network_nodes_reindexed(
+    DATA_DIR, nodes="small-nodes.csv", edge_table="small-edge-table.csv"
+) -> None:
+    """"""
+    nodes = pd.read_csv(DATA_DIR / nodes, delimiter=",")
+    edge_table = tuple(
+        t
+        for t in pd.read_csv(
+            DATA_DIR / edge_table, delimiter=",", header=None
+        ).itertuples(
+            index=False,
+            name=None,
+        )
+    )
+
+    nodes_before = pd.DataFrame(nodes)
+
+    G = Connections(edge_table, nodes)
+    nodes_after = G.nodes
+
+    # Don't drop any nodes
+    assert nodes_after.shape == nodes_before.shape
+
+    # Node mapping has taken place appropriately
+    for old_index in nodes_before.index:
+        new_index = nodes_after.index[old_index]
+
+        old_row = nodes_before.iloc[old_index]
+        new_row = nodes_after.iloc[new_index]
+
+        pd.testing.assert_series_equal(
+            old_row,
+            new_row,
+        )
