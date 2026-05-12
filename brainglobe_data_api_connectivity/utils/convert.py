af06aec24b58e6fd386eb67a47234d2f99b14bd3
Stella <30465823+stellaprins@users.noreply.github.com>
2026-05-12 12:46:04 +0100

    add lookup_and_morph_node_index docstring

--- brainglobe_data_api_connectivity/utils/convert.py
+++ brainglobe_data_api_connectivity/utils/convert.py
@@ -4,12 +4,38 @@ import numpy as np
 import pandas as pd
 
 
-def lookup_node_index(
+def lookup_and_morph_node_index(
     region_info: dict[str, Any],
     node_info: pd.DataFrame,
     region_to_node_heading: dict[str, str],
     morph_value: dict[str, Callable],
 ):
+    """look up `region_info` matching row index in `node_info`.
+
+    Each key–value pair in `region_info` is matched against the corresponding
+    column in `node_info`.
+
+    Column names may be remapped using `region_to_node_heading`, and values
+    may be transformed using functions provided in `morph_value`.
+
+    `node_info` is filtered until one matching row remains which index
+    is returned.
+
+
+    Parameters
+    region_info : dict[str, Any]
+        Mapping of region attributes to match against `node_info`.
+    node_info : pandas.DataFrame
+        Table of node information to search.
+    region_to_node_heading : dict[str, str]  (optional)
+        Mapping of `region_info` keys to `node_info` column names.
+    morph_value : dict[str, Callable] (optional)
+        Mapping of keys to functions that converts region values.
+
+    Returns
+    index: int
+        index of the unique matching row.
+    """
 
     possible_matches = node_info
 
