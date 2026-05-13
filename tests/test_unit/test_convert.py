0ffa15593e8de1f0e0b795a7da433f628272bba0
Stella <30465823+stellaprins@users.noreply.github.com>
2026-05-13 13:59:00 +0100

    add test_convert_matrix_to_edge_table

--- tests/test_unit/test_convert.py
+++ tests/test_unit/test_convert.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 import pytest
 
 from brainglobe_data_api_connectivity.utils.convert import (
+    convert_matrix_to_edge_table,
     lookup_and_morph_node_index,
 )
 
@@ -44,3 +46,55 @@ def test_lookup_node_index(info2morph, region_id, error, expected_idx):
             region_info, info, region_to_node_heading, morph_value
         )
         assert idx == expected_idx
+
+
+@pytest.mark.parametrize(
+    ["matrix", "expected_edge_table"],
+    [
+        pytest.param(
+            pd.DataFrame(
+                [
+                    [0, 1, 0],
+                    [0, 0, 2],
+                    [3, 4, 0],
+                ]
+            ),
+            np.array(
+                [
+                    [0, 1, 1],
+                    [1, 2, 2],
+                    [2, 0, 3],
+                    [2, 1, 4],
+                ]
+            ),
+            id="3x3 matrix with 4 edges",
+        ),
+        pytest.param(
+            pd.DataFrame(
+                [
+                    [0, 0],
+                    [0, 0],
+                ]
+            ),
+            np.empty((0, 3), dtype=int),
+            id="2x2 matrix with no edges",
+        ),
+        pytest.param(
+            pd.DataFrame(
+                [
+                    [0, 7],
+                    [0, 0],
+                ]
+            ),
+            np.array(
+                [
+                    [0, 1, 7],
+                ]
+            ),
+            id="2x2 matrix with single edge",
+        ),
+    ],
+)
+def test_convert_matrix_to_edge_table(matrix, expected_edge_table):
+    edge_table = convert_matrix_to_edge_table(matrix)
+    assert np.array_equal(edge_table, expected_edge_table)
