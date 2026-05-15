e79c097baa53ab2cd0ef3daa12be8f9b804ac1e3
Stella <30465823+stellaprins@users.noreply.github.com>
2026-05-15 17:59:33 +0100

    Add SWANSON_PARAMS dict on top for clarity
    As suggested by @willGraham01

--- scripts/swanson_datasets_to_csvs.py
+++ scripts/swanson_datasets_to_csvs.py
@@ -9,6 +9,7 @@ Excel files are updated.
 """
 
 from pathlib import Path
+from typing import TypedDict
 
 import numpy as np
 import pandas as pd
@@ -16,50 +17,66 @@ import pandas as pd
 from brainglobe_data_api_connectivity.io import excel, validate_input
 from brainglobe_data_api_connectivity.utils import convert, tidy
 
-EDGE_INFO_MORPH_DICT = {
-    "side": {"one": int(1), "two": int(2), "left": int(1), "right": int(2)}
+
+class SwansonParams(TypedDict):
+    matrix_file: Path
+    edge_info_file: Path
+    matrix_sheets: list[str]
+    matrix_range: tuple[str, str]
+    node_info_range: tuple[str, str]
+    mrcc_col: str
+    mrcc_row: int
+
+
+EDGE_INFO_MORPH_DICT = {"side": {"one": 1, "two": 2, "left": 1, "right": 2}}
+DATA_FOLDER = Path("data")
+SWANSON_PARAMS: SwansonParams = {
+    "matrix_file": DATA_FOLDER
+    / "swansonDatasetS3 CNS data matrices JHr1.xlsx",
+    "edge_info_file": DATA_FOLDER / "swansonDatasetS2 CNS CRs JHr1.xlsx",
+    "matrix_sheets": [f"CNS2{sex} modules" for sex in ["m", "f"]],
+    "matrix_range": ("T8", "AFU841"),
+    "node_info_range": ("A7", "S841"),
+    "mrcc_col": "P",
+    "mrcc_row": 5,
 }
 
 if __name__ == "__main__":
-    data_folder = Path("data")
-
-    # File paths
-    matrix_file = data_folder / "swansonDatasetS3 CNS data matrices JHr1.xlsx"
-    edge_information_file = data_folder / "swansonDatasetS2 CNS CRs JHr1.xlsx"
-
-    # Sheet and range definitions
-    matrix_sheets = [f"CNS2{sex} modules" for sex in ["m", "f"]]
-    data_range = ("T8", "AFU841")
-    info_range = ("A7", "S841")
-
-    #  MRCC (multiresolution consensus clustering number)
-    mrcc_col = "P"  # MRCC
-    mrcc_row = 5  # MRCC
-
     # Clean and save edge_information CSV
-    edge_info = pd.read_excel(edge_information_file, header=[0, 1])
+    edge_info = pd.read_excel(SWANSON_PARAMS["edge_info_file"], header=[0, 1])
     edge_info.columns = edge_info.columns.map(
         lambda x: "_".join([str(i) for i in x if "Unnamed:" not in i])
-    )  # combine multi-index headers
+    )
     edge_info.columns = tidy.rename_columns(edge_info.columns)
 
-    # different `region_side` labels are used ("one" "two" / "left" "right")
-    # mapping on `node_info` sides 1 and 2
     morph_dict = EDGE_INFO_MORPH_DICT["side"]
-
     for col in [c for c in edge_info.columns if "side" in c]:
         edge_info[col] = edge_info[col].map(morph_dict)
-    edge_info.to_csv(data_folder / "edge_info.csv", index=False)
 
-    for sheet in matrix_sheets:
+    edge_info.to_csv(DATA_FOLDER / "edge_info.csv", index=False)
+
+    # Process each matrix sheet
+    for sheet in SWANSON_PARAMS["matrix_sheets"]:
         # Load and validate matrix and ids
-        matrix = excel.get_df_from_excel(matrix_file, sheet, data_range)
+        matrix = excel.get_df_from_excel(
+            SWANSON_PARAMS["matrix_file"],
+            sheet,
+            SWANSON_PARAMS["matrix_range"],
+        )
+
         mrcc_row_values = excel.get_row_values(
-            matrix_file, data_range, mrcc_col, sheet
+            SWANSON_PARAMS["matrix_file"],
+            SWANSON_PARAMS["matrix_range"],
+            SWANSON_PARAMS["mrcc_col"],
+            sheet,
         )
         mrcc_col_values = excel.get_col_values(
-            matrix_file, data_range, mrcc_row, sheet
+            SWANSON_PARAMS["matrix_file"],
+            SWANSON_PARAMS["matrix_range"],
+            SWANSON_PARAMS["mrcc_row"],
+            sheet,
         )
+
         validate_input.check_ids(mrcc_row_values, mrcc_col_values)
         validate_input.validate_matrix(
             matrix, mrcc_row_values, mrcc_col_values
@@ -69,25 +86,28 @@ if __name__ == "__main__":
         edge_table = convert.convert_matrix_to_edge_table(matrix)
 
         # Load and tidy node info
-        info = excel.get_df_from_excel(
-            matrix_file, sheet_name=sheet, data_range=info_range, header=0
+        node_info = excel.get_df_from_excel(
+            SWANSON_PARAMS["matrix_file"],
+            sheet_name=sheet,
+            data_range=SWANSON_PARAMS["node_info_range"],
+            header=0,
         )
-        info.rename(columns={info.columns[0]: "Side"}, inplace=True)
-        info.columns = tidy.rename_columns(info.columns)
+        node_info.rename(columns={node_info.columns[0]: "Side"}, inplace=True)
+        node_info.columns = tidy.rename_columns(node_info.columns)
 
         # Save outputs
         sheet_tag = sheet.replace(" ", "_")
         np.savetxt(
-            data_folder / f"{sheet_tag}_edge_table.csv",
+            DATA_FOLDER / f"{sheet_tag}_edge_table.csv",
             edge_table,
             fmt="%d",
             delimiter=",",
         )
-        info.to_csv(data_folder / f"{sheet_tag}_info.csv", index=False)
+        node_info.to_csv(DATA_FOLDER / f"{sheet_tag}_info.csv", index=False)
 
     # Consolidate duplicate info files
     tidy.consolidate_duplicates(
         pattern="[!edge]*_info.csv",
-        folder=data_folder,
+        folder=DATA_FOLDER,
         output_name="node_info.csv",
     )
