fbeb68536170c29c12ec7616b9854d986a26c769
Stella <30465823+stellaprins@users.noreply.github.com>
2026-06-01 09:18:49 +0100

    Add optional setting for inclusion of zero entries in  convert_matrix_to_edge_tab

--- brainglobe_data_api_connectivity/utils/convert.py
+++ brainglobe_data_api_connectivity/utils/convert.py
@@ -2,14 +2,18 @@ import numpy as np
 import pandas as pd
 
 
-def convert_matrix_to_edge_table(matrix: pd.DataFrame) -> np.ndarray:
+def convert_matrix_to_edge_table(
+    matrix: pd.DataFrame, include_zeros: bool = False
+) -> np.ndarray:
     """Convert adjacency matrix into an edge table.
-
-    Only non‑zero entries in are interpreted as edges.
 
     Parameters
     matrix : pandas.DataFrame
         Adjacency matrix.
+
+    include_zeros : bool (default=False)
+        - If True: use only the non-zero entries of the matrix
+        - If False: use all entries of the matrix
 
     Returns
     edge_table: numpy.ndarray
@@ -19,7 +23,14 @@ def convert_matrix_to_edge_table(matrix: pd.DataFrame) -> np.ndarray:
             - 3. edge weight
     """
     adjacency_matrix = matrix.to_numpy()
-    connections = adjacency_matrix.nonzero()
-    weights = adjacency_matrix[connections]
-    edge_table = np.column_stack(connections + (weights,))
+    if include_zeros is False:
+        connections = matrix.to_numpy().nonzero()
+        weights = adjacency_matrix[connections]
+        edge_table = np.column_stack(connections + (weights,))
+    else:
+        rows, cols = np.indices(adjacency_matrix.shape)
+        weights = adjacency_matrix[rows, cols]
+        edge_table = np.column_stack(
+            (rows.ravel(), cols.ravel(), weights.ravel())
+        )
     return edge_table
