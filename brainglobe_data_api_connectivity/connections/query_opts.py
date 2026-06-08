04287ebd5af566db5ce4a64fc358a583a60a1f77
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-08 13:47:32 +0100

    Move query options to their own file for config and org purposes

---
+++ brainglobe_data_api_connectivity/connections/query_opts.py
@@ -0,0 +1,40 @@
+from enum import IntEnum, StrEnum
+
+
+class NodeInConnection(StrEnum):
+    """
+    Options for node roles in a connection.
+
+    Edges (connections) are directed, which means that when one asks questions
+    about the direct connections that a node has, one may optionally want to
+    specify whether the node in question is the `input` (or "source") node in
+    the connection, the `output` (or "target") node in the connection, or
+    either.
+
+    These options are standardised as `StrEnum`s to avoid potentially diverging
+    string and language conventions in the API.
+    """
+
+    EITHER = "either"
+    INPUT = "source"
+    OUTPUT = "target"
+
+
+class ConnectionsLookup(IntEnum):
+    """
+    Options flag for querying reported connections.
+
+    The network objects that are constructed by the API use so-called
+    "reported" connection data, which is essentially the information per
+    connection that is deemed the most accurate or reliable. By contrast, the
+    edge information attached to a `Connections` object may contain multiple
+    reports for the same connection, with different levels of strength and
+    accuracy of the reported result. One must decide from which source to draw
+    information about the connections when posing queries about them.
+
+    These options are standardised as `IntEnum`s to avoid potentially diverging
+    conventions in the API.
+    """
+
+    ALL = 0
+    REPORTED = 1
