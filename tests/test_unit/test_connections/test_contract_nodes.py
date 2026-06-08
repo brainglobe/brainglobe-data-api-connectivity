66de2e59cf35d12c4d4378c13f5015e7abbf645f
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-08 11:06:13 +0100

    Smaller files = better

--- tests/test_unit/test_connections/test_contract_nodes.py
+++ tests/test_unit/test_connections/test_contract_nodes.py
@@ -1,17 +1,10 @@
-from collections.abc import Callable
 from pathlib import Path
-from typing import Any, Iterable
+from typing import Any
 
 import polars as pl
 import pytest
 
 from brainglobe_data_api_connectivity.connections import Connections
-from brainglobe_data_api_connectivity.connections.node_contractions import (
-    average_of_weights,
-    max_of_weights,
-    min_of_weights,
-    sum_of_weights,
-)
 
 
 @pytest.mark.parametrize(
@@ -167,32 +160,3 @@ def test_contract_nodes_provide_information(
             )
         else:
             assert stored_super_node_info[col_name] == super_node_index
-
-
-@pytest.mark.parametrize(
-    ("fn", "input_weights", "expected_weight"),
-    [
-        pytest.param(
-            average_of_weights, [0, 0.5, 1, 1.5], 3.0 / 4.0, id="average"
-        ),
-        pytest.param(max_of_weights, [0, 0.5, 1, 1.5], 1.5, id="max"),
-        pytest.param(min_of_weights, [0, 0.5, 1, 1.5], 0.0, id="min"),
-        pytest.param(sum_of_weights, [0, 0.5, 1, 1.5], 3.0, id="sum"),
-    ],
-)
-def test_weight_contraction_functions(
-    fn: Callable[..., float],
-    input_weights: Iterable[float],
-    expected_weight: Iterable[float],
-) -> None:
-    """
-    Short test function to provide coverage for the packaged
-    edge-weight-contraction functions.
-
-    This mainly exists to keep test coverage happy, as the majority of
-    contraction functions just use Python builtins under the hood. Still,
-    it checks that we are using the correct signature, and for more complex
-    contraction functions we will want to check the logic is being correctly
-    executed.
-    """
-    assert fn(*input_weights) == expected_weight
