3618f0214ed87f50072ce69d9b527adb0136ca29
Stella <30465823+stellaprins@users.noreply.github.com>
2026-06-19 13:15:53 +0100

    rename module to match updated class name

--- brainglobe_data_api_connectivity/dijkstra/strategy/__init__.py
+++ brainglobe_data_api_connectivity/dijkstra/strategy/__init__.py
@@ -1,5 +1,5 @@
 from ._base import DijkstraStrategy
-from .shortest_distance import LowestCost
+from .lowest_cost import LowestCost
 from .widest_path import WidestPath
 
 __all__ = ("DijkstraStrategy", "LowestCost", "WidestPath")
