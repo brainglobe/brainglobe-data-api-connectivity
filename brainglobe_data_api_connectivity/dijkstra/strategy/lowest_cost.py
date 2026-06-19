31531cee0d016d5fa0374d2170d69b6aaff83cce
Stella <30465823+stellaprins@users.noreply.github.com>
2026-06-19 15:40:05 +0100

    add LowestCost docstring

--- brainglobe_data_api_connectivity/dijkstra/strategy/lowest_cost.py
+++ brainglobe_data_api_connectivity/dijkstra/strategy/lowest_cost.py
@@ -2,7 +2,15 @@ from ._base import DijkstraStrategy
 
 
 class LowestCost(DijkstraStrategy[float]):
-    """"""
+    """Strategy that finds the path with the lowest cost.
+
+    Costs is determined by adding edge weights, lower totals are considered
+    better.
+
+    If edge weights reflect connection strength between brain regions,
+    this strategy returns the path and cost of the path with the weakest
+    total strength.
+    """
 
     def _cost_to(self, current_cost: float, next_edge_weight: float) -> float:
         return current_cost + next_edge_weight
