8ea1b7e9de48a32123e6a47cccc300a084429776
Stella <30465823+stellaprins@users.noreply.github.com>
2026-06-19 13:30:22 +0100

    add FewestSteps docstring

--- brainglobe_data_api_connectivity/dijkstra/strategy/fewest_steps.py
+++ brainglobe_data_api_connectivity/dijkstra/strategy/fewest_steps.py
@@ -2,7 +2,14 @@ from ._base import DijkstraStrategy
 
 
 class FewestSteps(DijkstraStrategy[float]):
-    """"""
+    """Strategy that finds the path with the fewest number of steps.
+
+    Cost increases by 1 for every move, and lower costs are considered better.
+
+    In a network where edges represent connections between brain regions,
+    this strategy finds the path that passes through the smallest number of
+    regions.
+    """
 
     def _cost_to(self, current_cost: float, next_edge_weight: float) -> float:
         return current_cost + 1
